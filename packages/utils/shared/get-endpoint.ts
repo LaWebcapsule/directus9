c7f62b6bb22958356f57fe213bde8249e17a5ac1
WBCE_local <WBCE_builder@local>
2024-01-10 13:40:56 +0100

    Full fix and comments

--- packages/utils/shared/get-endpoint.ts
+++ packages/utils/shared/get-endpoint.ts
@@ -1,4 +1,5 @@
 export function getEndpoint(collection: string): string {
+	// [issues-33] we allow directus_collections to be read as items for m2m collection visualization
 	if (collection == 'directus_collections') {
 		return `/items/${collection}`;
 	}
