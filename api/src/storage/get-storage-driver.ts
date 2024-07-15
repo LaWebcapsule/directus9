f076bdccfd01f11e9d49cdae6b693d395d83f5a5
Will K <dev@pxslip.com>
2024-07-15 19:03:40 +0000

    Re-add the s3 driver

--- api/src/storage/get-storage-driver.ts
+++ api/src/storage/get-storage-driver.ts
@@ -2,7 +2,7 @@ import type { Driver } from '@db-studio/storage';
 
 export const _aliasMap: Record<string, string> = {
 	local: '@db-studio/storage-driver-local',
-	// s3: '@db-studio/storage-driver-s3',
+	s3: '@db-studio/storage-driver-s3',
 	gcs: '@db-studio/storage-driver-gcs',
 	azure: '@db-studio/storage-driver-azure',
 	cloudinary: '@db-studio/storage-driver-cloudinary',
