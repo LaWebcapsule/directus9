aae9b2d38cde885a112170f4b637215266de6c50
Will K <dev@pxslip.com>
2024-08-16 15:22:41 +0000

    Updating the startup log

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
-
 import { getVueComponentName } from '@/utils/get-vue-component-name';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './app.vue';
 import { registerComponents } from './components/register';
-import { DIRECTUS_LOGO } from './constants';
 import { registerDirectives } from './directives/register';
 import { i18n } from './lang/';
 import { router } from './router';
@@ -18,16 +16,10 @@ init();
 async function init() {
 	const version = __DIRECTUS_VERSION__;
 
-	console.log(DIRECTUS_LOGO);
-
-	console.info(
-		`Hey! Interested in helping build this open-source data management platform?\nIf so, join our growing team of contributors at: https://directus.chat`
-	);
-
 	if (import.meta.env.DEV) {
-		console.info(`%c🐰 Starting Directus v${version}...`, 'color:Green');
+		console.info(`%c🐰 Starting DB Studio v${version}...`, 'color:Green');
 	} else {
-		console.info(`%c🐰 Starting Directus...`, 'color:Green');
+		console.info(`%c🐰 Starting DB Studio...`, 'color:Green');
 	}
 
 	console.time('🕓 Application Loaded');
