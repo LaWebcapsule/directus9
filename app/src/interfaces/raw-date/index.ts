7968ff997060147cdc21b5ea592d1d06c147db3b
Will K <dev@pxslip.com>
2024-08-20 19:54:14 +0000

    Create a  working raw-date interface

--- app/src/interfaces/raw-date/index.ts
+++ app/src/interfaces/raw-date/index.ts
@@ -1,51 +1,35 @@
 import { defineInterface } from '@db-studio/utils';
 import RawDate from './raw-date.vue';
-import PreviewSVG from './preview.svg?raw';
 
 export default defineInterface({
-	id: 'datetime',
-	name: '$t:interfaces.datetime.datetime',
-	description: '$t:interfaces.datetime.description',
+	id: 'raw-date',
+	name: '$t:interfaces.raw-date.raw-date',
+	description: '$t:interfaces.raw-date.description',
 	icon: 'today',
 	component: RawDate,
 	types: ['dateTime', 'date', 'time', 'timestamp'],
 	group: 'selection',
-	options: ({ field }) => {
-		if (field.type === 'date') {
-			if (field.meta?.options) {
-				field.meta.options = {};
-			}
-
-			return [];
-		}
-
+	options: () => {
 		return [
 			{
-				field: 'includeSeconds',
-				name: '$t:interfaces.datetime.include_seconds',
-				type: 'boolean',
+				field: 'validationRegex',
+				name: '$t:interfaces.raw-date.regex',
+				type: 'string',
 				meta: {
-					width: 'half',
-					interface: 'boolean',
-				},
-				schema: {
-					default_value: false,
+					width: 'full',
+					interface: 'input',
 				},
 			},
 			{
-				field: 'use24',
-				name: '$t:interfaces.datetime.use_24',
-				type: 'boolean',
+				field: 'validationMessage',
+				name: '$t:interfaces.raw-date.message',
+				type: 'string',
 				meta: {
-					width: 'half',
-					interface: 'boolean',
-				},
-				schema: {
-					default_value: true,
+					width: 'full',
+					interface: 'input',
 				},
 			},
 		];
 	},
 	recommendedDisplays: ['datetime'],
-	preview: PreviewSVG,
 });
