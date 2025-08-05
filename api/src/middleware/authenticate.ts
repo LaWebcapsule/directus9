e4dc5153073ac3d9b7a5eadfeda20a7dabdf2755
Sofia Chakir <sofiachakir@gmail.com>
2025-08-05 11:39:02 +0100

    Fix Token Expired Exception

--- api/src/middleware/authenticate.ts
+++ api/src/middleware/authenticate.ts
@@ -4,11 +4,12 @@ import { isEqual } from 'lodash-es';
 import getDatabase from '../database/index.js';
 import emitter from '../emitter.js';
 import env from '../env.js';
-import { InvalidCredentialsException, TokenExpiredException } from '../exceptions/index.js';
+import { InvalidCredentialsException } from '../exceptions/index.js';
 import asyncHandler from '../utils/async-handler.js';
 import { getIPFromReq } from '../utils/get-ip-from-req.js';
 import isDirectusJWT from '../utils/is-directus-jwt.js';
 import { verifyAccessJWT } from '../utils/jwt.js';
+import { ACCESS_COOKIE_CLEAR_OPTIONS, REFRESH_COOKIE_CLEAR_OPTIONS } from '../constants.js';
 
 /**
  * Verify the passed JWT and assign the user ID and role to `req`
@@ -48,56 +49,75 @@ export const handler = async (req: Request, _res: Response, next: NextFunction) 
 		return next();
 	}
 
-	req.accountability = defaultAccountability;
+	try {
+		req.accountability = defaultAccountability;
 
-	if (req.token) {
-		if (isDirectusJWT(req.token)) {
-			const payload = verifyAccessJWT(req.token, env['SECRET']);
+		if (req.token) {
+			if (isDirectusJWT(req.token)) {
+				const payload = verifyAccessJWT(req.token, env['SECRET']);
 
-			// Try finding the user with a valid refresh token stored in the access token
-			const user = await database
-				.select({
-					session_expires: 's.expires',
-					user_id: 'u.id',
-				})
-				.from('directus_sessions AS s')
-				.leftJoin('directus_users AS u', 's.user', 'u.id')
-				.where('s.token', payload.refresh_token)
-				.andWhere('s.expires', '>=', new Date())
-				.first();
+				// Try finding the user with a valid refresh token stored in the access token
+				const user = await database
+					.select({
+						session_expires: 's.expires',
+						user_id: 'u.id',
+					})
+					.from('directus_sessions AS s')
+					.leftJoin('directus_users AS u', 's.user', 'u.id')
+					.where('s.token', payload.refresh_token)
+					.andWhere('s.expires', '>=', new Date())
+					.first();
 
-			if (!user) {
-				throw new TokenExpiredException();
+				if (!user) {
+					throw new InvalidCredentialsException();
+				}
+
+				req.accountability.role = payload.role;
+				req.accountability.admin = payload.admin_access === true || payload.admin_access == 1;
+				req.accountability.app = payload.app_access === true || payload.app_access == 1;
+
+				if (payload.share) req.accountability.share = payload.share;
+				if (payload.share_scope) req.accountability.share_scope = payload.share_scope;
+				if (payload.id) req.accountability.user = payload.id;
+			} else {
+				// Try finding the user with the provided token
+				const user = await database
+					.select(
+						'directus_users.id',
+						'directus_users.role',
+						'directus_roles.admin_access',
+						'directus_roles.app_access'
+					)
+					.from('directus_users')
+					.leftJoin('directus_roles', 'directus_users.role', 'directus_roles.id')
+					.where({
+						'directus_users.token': req.token,
+						status: 'active',
+					})
+					.first();
+
+				if (!user) {
+					throw new InvalidCredentialsException();
+				}
+
+				req.accountability.user = user.id;
+				req.accountability.role = user.role;
+				req.accountability.admin = user.admin_access === true || user.admin_access == 1;
+				req.accountability.app = user.app_access === true || user.app_access == 1;
 			}
-
-			req.accountability.role = payload.role;
-			req.accountability.admin = payload.admin_access === true || payload.admin_access == 1;
-			req.accountability.app = payload.app_access === true || payload.app_access == 1;
-
-			if (payload.share) req.accountability.share = payload.share;
-			if (payload.share_scope) req.accountability.share_scope = payload.share_scope;
-			if (payload.id) req.accountability.user = payload.id;
-		} else {
-			// Try finding the user with the provided token
-			const user = await database
-				.select('directus_users.id', 'directus_users.role', 'directus_roles.admin_access', 'directus_roles.app_access')
-				.from('directus_users')
-				.leftJoin('directus_roles', 'directus_users.role', 'directus_roles.id')
-				.where({
-					'directus_users.token': req.token,
-					status: 'active',
-				})
-				.first();
-
-			if (!user) {
-				throw new InvalidCredentialsException();
-			}
-
-			req.accountability.user = user.id;
-			req.accountability.role = user.role;
-			req.accountability.admin = user.admin_access === true || user.admin_access == 1;
-			req.accountability.app = user.app_access === true || user.app_access == 1;
 		}
+	} catch (error) {
+		if (error instanceof InvalidCredentialsException) {
+			if (req?.cookies[env['ACCESS_TOKEN_COOKIE_NAME']]) {
+				_res?.clearCookie(env['ACCESS_TOKEN_COOKIE_NAME'], ACCESS_COOKIE_CLEAR_OPTIONS);
+			}
+
+			if (req?.cookies[env['REFRESH_TOKEN_COOKIE_NAME']]) {
+				_res?.clearCookie(env['REFRESH_TOKEN_COOKIE_NAME'], REFRESH_COOKIE_CLEAR_OPTIONS);
+			}
+		}
+
+		throw error;
 	}
 
 	return next();
